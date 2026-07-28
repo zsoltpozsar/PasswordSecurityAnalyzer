0bb71b69fb85ee79a9548e0788dae7c4dbce6a0e
zsoltpozsar <daimamaster5@gmail.com>
2026-07-28 15:30:32 +0200

    Add basic password validation

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ import tkinter as tk
 # Főablak létrehozása
 window = tk.Tk()
 
+# A jelszó elemzéséhez szükséges függvény
+def analyze_password():
+
+    # A beviteli mező tartalmának kiolvasása
+    password = password_entry.get()
+
+    # Ellenőrizzük, hogy üres-e a mező
+    if password == "":
+        result_label.config(
+            text="Bitte geben Sie ein Passwort ein!",
+            fg="red"
+        )
+    else:
+        result_label.config(
+            text="Das Passwort wurde erkannt.",
+            fg="green"
+        )
+
 # Ablak címe
 window.title("Password Security Analyzer")
 
@@ -28,9 +46,13 @@ password_entry.pack(pady=10)
 # Gomb
 analyze_button = tk.Button(
     window,
-    text="Passwort analysieren"
+    text="Passwort analysieren",
+    command=analyze_password
 )
 analyze_button.pack(pady=20)
+
+result_label = tk.Label(window, text="", font=("Arial", 12))
+result_label.pack(pady=10)
 
 # Program indítása
 window.mainloop()
